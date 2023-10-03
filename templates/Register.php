50db8762a3cab7c8d3abc23690a38ea2855ba5ac
0KeTeR0 <louis.demeocq@gmail.com>
2023-10-03 14:58:28 +0200

    Add code structure to Register.php

--- templates/Register.php
+++ templates/Register.php
@@ -1,1 +1,35 @@
-<?php
+<?php $title = "Page d'accueil"; ?>
+<?php ob_start() ?>
+
+    <main>
+        <section class="choix_etudiant_senior">
+            <div>
+                <div>
+                    <h1>Création de profil</h1>
+                </div>
+                <div>
+                    <p>Je suis</p>
+                </div>
+                <div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="Form_etudiant">
+
+        </section>
+
+        <section class="Form_Senior">
+
+        </section>
+    </main>
+
+
+<?php $content = ob_get_clean(); ?>
+<?php require('layout.php'); ?>
