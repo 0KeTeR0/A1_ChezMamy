2c73675d595e9ec3b73803475037e899edda1bdc
Akt0o <valentin.colindre@gmail.com>
2023-10-03 16:08:10 +0200

    Register Back : added post input.

--- src/Controllers/RegisterController.php
+++ src/Controllers/RegisterController.php
@@ -8,8 +8,29 @@ use App\ChezMamy\Model\Register;
 
 class RegisterController
 {
-    public function execute()
+    public function execute(?array $input)
     {
+        if(count($input) > 0) {
+            $username = null;
+            $password = null;
+            $passwordRepeat = null;
+
+            if(!empty($input['username']) && !empty($input['password']) && !empty($input['passwordRepeat']))
+            {
+                $username = htmlspecialchars(strip_tags($input['username']));
+                $password = $input['password'];
+                $passwordRepeat = $input['passwordRepeat'];
+            }
+            else throw new \Exception("Veuillez remplir tous les champs.");
+
+            $register = new Register($username, $password, $passwordRepeat);
+            $res = $register->execute();
+            if(!$res['success']) throw new \Exception($res['message']);
+            else {
+                header('Location: index.php?action=login');
+            }
+        }
+
         require('templates/Register.php');
     }
 }
