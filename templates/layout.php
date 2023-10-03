141b3d2fc9ef0f51f7f0b4c84dcdd5f51f6b5aa6
Romain Card <romain.card9@gmail.com>
2023-10-03 11:51:21 +0200

    added questions to faq and fix footer

--- templates/layout.php
+++ templates/layout.php
@@ -51,14 +51,18 @@
                 <br><br>
                 Si vous avez plus de 30 ans et moins de 60 ans, nous vous invitons à vous rendre sur le site "La Carte des Colocs" ou encore "Cooloc". Ils proposent de la cohabitation, des chambres chez l'habitant ou encore de la colocation, pour tous les âges.</dd>
             <hr>
-            <dt class="question">Question : blabla <span><svg width="67" height="65" viewBox="0 0 67 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0586 41.6412L33.5796 23.36L52.1007 41.6412" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" stroke-linecap="square"/></svg></span></dt>
-            <dd class="answer">Réponse à la questiooooooooon : Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam architecto at dignissimos eaque eius ex exercitationem harum maiores molestias mollitia natus nesciunt obcaecati officiis perferendis rem ullam, veniam vitae.</dd>
+            <dt class="question">Puis-je m'inscrire tout au long de l'année ?<span><svg width="67" height="65" viewBox="0 0 67 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0586 41.6412L33.5796 23.36L52.1007 41.6412" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" stroke-linecap="square"/></svg></span></dt>
+            <dd class="answer">Oui, bien sûr. Il vous suffit d’enregistrer votre profil sur notre site tout simplement, gratuitement et en quelques clics. Vous pouvez aussi créer votre profil maintenant avec une date de disponibilité ou de recherche ultérieure.</dd>
             <hr>
-            <dt class="question">Question : blabla <span><svg width="67" height="65" viewBox="0 0 67 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0586 41.6412L33.5796 23.36L52.1007 41.6412" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" stroke-linecap="square"/></svg></span></dt>
-            <dd class="answer">Réponse à la questiooooooooon : Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam architecto at dignissimos eaque eius ex exercitationem harum maiores molestias mollitia natus nesciunt obcaecati officiis perferendis rem ullam, veniam vitae.</dd>
+            <dt class="question">Comment se matérialise la cohabitation intergénérationnelle ?<span><svg width="67" height="65" viewBox="0 0 67 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0586 41.6412L33.5796 23.36L52.1007 41.6412" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" stroke-linecap="square"/></svg></span></dt>
+            <dd class="answer">Nous rédigeons un contrat de cohabitation intergénérationnelle, qui n’est pas un contrat de bail, mais une charte du bien vivre ensemble, qui reprend l’intégralité des conditions organisationnelles, relationnelles et financières établies entre chaque partie. Ce contrat vous donne droit aux APL en tant qu'étudiants / jeunes actifs et a une exonération d'impots sur les montants perçus pour le logis si vous êtes une personne de plus de 60 ans.</dd>
             <hr>
-            <dt class="question">Question : blabla <span><svg width="67" height="65" viewBox="0 0 67 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0586 41.6412L33.5796 23.36L52.1007 41.6412" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" stroke-linecap="square"/></svg></span></dt>
-            <dd class="answer">Réponse à la questiooooooooon : Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam architecto at dignissimos eaque eius ex exercitationem harum maiores molestias mollitia natus nesciunt obcaecati officiis perferendis rem ullam, veniam vitae.</dd>
+            <dt class="question">Puis-je recevoir un jeune si je suis locataire?<span><svg width="67" height="65" viewBox="0 0 67 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0586 41.6412L33.5796 23.36L52.1007 41.6412" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" stroke-linecap="square"/></svg></span></dt>
+            <dd class="answer">Oui, nous vous fournirons une lettre à remettre à votre propriétaire pour lui signaler votre cohabitation avec un(e) jeune, mais, celui-ci ne peut s'y opposer (contrairement à une sous-location ou une collocation).
+                <br><br>
+                Que vous soyez locataire dans un parc privé, public ou social, le concept de cohabitation intergénérationnelle et solidaire vous est possible.
+                <br><br>
+                Cf article L. 631-17 de la loi de 2018 portant sur l'évolution du logement, de l'aménagement et du numérique.</dd>
         </dl>
     </section>
 
@@ -88,11 +92,13 @@
                     </ul>
                 </li>
                 <li><p>Nos réseaux</p>
-                    <ul>
-                        <li><a id="twitterFooter" href=""><img src="../assets/img/twitter.png"></a></li>
-                        <li><a id="facebookFooter" href=""><img src="../assets/img/facebook.png"></a></li>
-                        <li><a id="snapchatFooter" href=""><img src="../assets/img/snapchat.png"> </a></li>
-                    </ul>
+                    <div>
+                        <ul>
+                            <li><a id="twitterFooter" href=""><img src="../assets/img/twitter.png"></a></li>
+                            <li><a id="facebookFooter" href=""><img src="../assets/img/facebook.png"></a></li>
+                            <li><a id="snapchatFooter" href=""><img src="../assets/img/snapchat.png"> </a></li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
             <div><p>&copy; 2023 Tous droits réservés</p></div>
