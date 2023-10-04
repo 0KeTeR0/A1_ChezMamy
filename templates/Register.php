2aaf72126e1ffde21f51b87929aaefca1ee02c93
Synxgz <47689092+Synxgz@users.noreply.github.com>
2023-10-04 11:12:50 +0200

    css register form

--- templates/Register.php
+++ templates/Register.php
@@ -10,7 +10,7 @@
                 </div>
                 <div>
                     <div id="registerChoice">
-                        <p><b>Je suis...</b></p>
+                        <label>Je suis...</label>
                         <div id="jeSuisEtudiant" class="bouton">Étudiant à la recherche d’un logement</div>
                         <div id="jeSuisSenior" class="bouton">Sénior avec un logement à partager</div>
                     </div>
@@ -22,49 +22,49 @@
 
                         <div id="Etudiant_etape1" class="form-group">
                             <h2>Identité</h2>
-                            <div>
-                                <label for="fname">Prénom :</label>
+                            <div class="form-pair">
+                                <label for="fname">Prénom</label>
                                 <input type="text" id="fname" name="first_name" autofocus required>
                             </div>
-                            <div>
-                                <label for="lname">Nom :</label>
+                            <div class="form-pair">
+                                <label for="lname">Nom</label>
                                 <input type="text" id="lname" name="last_name" required>
                             </div>
-                            <div>
-                                <label for="bdate">Date de naissance :</label>
+                            <div class="form-pair">
+                                <label for="bdate">Date de naissance</label>
                                 <input type="date" id="bdate" name="date_of_birth" max="2008-01-01" required>
                             </div>
-                            <div>
-                                <label for="nationalité">Nationalité :</label>
+                            <div class="form-pair">
+                                <label for="nationalité">Nationalité</label>
                                 <input type="text" name="nationality" id="nationalité" required>
                             </div>
-                            <div>
-                                <label for="num">Téléphone :</label>
+                            <div class="form-pair">
+                                <label for="num">Téléphone</label>
                                 <input type="tel" id="num" name="phone" placeholder="Au format 00-00-00-00-00" pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}" required>
                             </div>
-                            <div>
-                                <label for="mail">Adresse email <i>(identifiant de connexion)</i> :</label>
+                            <div class="form-pair">
+                                <label for="mail">Adresse email <i>(identifiant de connexion)</i></label>
                                 <input type="email" id="mail" name="email" placeholder="exemple@gmail.com" required>
                             </div>
-                            <div>
-                                <label for="AdressePrt">Adresse des parents :</label>
+                            <div class="form-pair">
+                                <label for="AdressePrt">Adresse des parents</label>
                                 <input type="text" name="parents_address" id="AdressePrt" required>
                             </div>
-                            <div>
-                                <label for="residence">Ville :</label>
+                            <div class="form-pair">
+                                <label for="residence">Ville</label>
                                 <input type="text" id="residence" name="city" required>
                             </div>
-                            <div>
-                                <label for="postal">Code postal :</label>
+                            <div class="form-pair">
+                                <label for="postal">Code postal</label>
                                 <input type="text" id="postal" name="postal_code" title="doit comporter 5 chiffres" pattern="[0-9]{5}" required>
                             </div>
 
-                            <div>
+                            <div class="form-pair">
                                 <label for="knowAssociation">Comment avez-vous connu notre association ?</label>
                                 <input type="text" id="knowAssociation" name="know_association" required>
                             </div>
 
-                            <div>
+                            <div class="form-pair">
                                 <label for="motivation">Vos motivations pour choisir ce mode de logement</label>
                                 <textarea name="motivation" id="motivation" cols="4" rows="2" required></textarea>
                             </div>
@@ -77,24 +77,24 @@
                             <div>
                                 <h2>Etudes / stages</h2>
                             </div>
-                            <div>
-                                <label for="education_level">Collégiale et cycle supérieur :</label>
+                            <div class="form-pair">
+                                <label for="education_level">Collégiale et cycle supérieur</label>
                                 <input type="text" name="education_level" id="education_level">
                             </div>
-                            <div>
-                                <label for="stages">Stages, préciser :</label>
+                            <div class="form-pair">
+                                <label for="stages">Stages, préciser</label>
                                 <input type="text" name="internships" id="stages">
                             </div>
-                            <div>
-                                <label for="etablissement">Etablissement d'enseignement :</label>
+                            <div class="form-pair">
+                                <label for="etablissement">Etablissement d'enseignement</label>
                                 <input type="text" name="establishment" id="etablissement">
                             </div>
-                            <div>
-                                <label for="end_of_studies">Durée d'étude restante :</label>
+                            <div class="form-pair">
+                                <label for="end_of_studies">Durée d'étude restante</label>
                                 <input type="number" name="end_of_studies" id="end_of_studies" value="0" min="0" max="12">
                             </div>
-                            <div>
-                                <label for="date_of_arrival">Si vous êtes nouveau venu dans notre région, précisez votre date d'arrivée :</label>
+                            <div class="form-pair">
+                                <label for="date_of_arrival">Si vous êtes nouveau venu dans notre région, précisez votre date d'arrivée</label>
                                 <input type="date" name="date_of_arrival" id="date_of_arrival" min="<?= date('Y/m/d') ?>" > <!-- mettre la date actuelle en PHP pour min-->
                             </div>
                             <div>
@@ -107,43 +107,42 @@
                                 <h2>Mieux vous connaître</h2>
                             </div>
                             <div class="radio-group-1">
-                                <label for="fumeur">Êtes-vous fumeur ?</label>
-                                <div>
-                                    <input type="radio" id="fumeur_oui" name="is_smoking" value="oui">
+                                <div class="form-pair">
+                                    <label for="fumeur">Êtes-vous fumeur ?</label>
+                                    <input type="radio" id="fumeur_oui" name="is_smoking" value="1">
                                     <label for="fumeur_oui">Oui</label>
-                                </div>
-                                <div>
-                                    <input type="radio" id="fumeur_non" name="is_smoking" value="non">
+                                    <input type="radio" id="fumeur_non" name="is_smoking" value="0">
                                     <label for="fumeur_non">Non</label>
                                 </div>
-
-                                <label for="allergie">Avez-vous des allergies ?</label>
-                                <input type="radio" id="allergie_oui" name="is_allergic" value="oui">
-                                <label for="allergie_oui">Oui</label>
-                                <input type="radio" id="allergie_non" name="is_allergic" value="non">
-                                <label for="allergie_non">Non</label>
+                                <div class="form-pair">
+                                    <label for="allergie">Avez-vous des allergies ?</label>
+                                    <input type="radio" id="allergie_oui" name="is_allergic" value="1">
+                                    <label for="allergie_oui">Oui</label>
+                                    <input type="radio" id="allergie_non" name="is_allergic" value="0">
+                                    <label for="allergie_non">Non</label>
+                                </div>
                             </div>
-                            <div>
-                                <label for="allergique">Si oui, précisé :</label>
+                            <div class="form-pair">
+                                <label for="allergique">Précisé</label>
                                 <input type="search" name="allergies" id="allergique">
                             </div>
-                            <div>
+                            <div class="form-pair">
                                 <label for="permis">Êtes-vous titulaire d'un permis de conduire ?</label>
-                                <input type="radio" id="oui" name="can_drive" value="oui">
+                                <input type="radio" id="oui" name="can_drive" value="1">
                                 <label for="oui">Oui</label>
-                                <input type="radio" id="non" name="can_drive" value="non">
+                                <input type="radio" id="non" name="can_drive" value="0">
                                 <label for="non">Non</label>
                             </div>
 
-                            <div>
+                            <div class="form-pair">
                                 <label for="locomotion">Si vous avez un moyen de locomotion, précisez: </label>
                                 <input type="text" name="means_of_locomotion" id="locomotion">
                             </div>
-                            <div>
-                                <label for="centresInteret">Vos centres d'intérêts majeurs :</label>
+                            <div class="form-pair">
+                                <label for="centresInteret">Vos centres d'intérêts majeurs</label>
                                 <input type="text" name="interests" id="centresInteret">
                             </div>
-                            <div>
+                            <div class="form-pair">
                                 <label for="but">Qu'est-ce qui vous pousse à rechercher la cohabitation avec une personne âgée ?</label>
                                 <input type="text" name="why" id="but">
                             </div>
@@ -158,17 +157,17 @@
                             </div>
 
                             <div class="form-group">
-                                <div>
+                                <div class="form-pair">
                                     <input type="radio" name="housing" id="lgmtGratuit" value="1" required>
                                     <label for="lgmtGratuit">1-logement gratuit, en échange de présence soirs et nuits.</label>
                                     <span>Vos journées sont libres. Vous êtes présent le soir à l'heure du repas excepté une soirée par semaine, deux week-ends par mois du vendredi soir au dimanche soir et trois semaines de vacances entre septembre et juin.</span>
                                 </div>
-                                <div>
+                                <div class="form-pair">
                                     <input type="radio" name="housing" id="lgmtEco+" value="2" required>
                                     <label for="lgmtEco+">2-Logement économique, avec une participation aux frais d'usage et d'échange de services</label>
                                     <span>Vous avez du temps et de la disponibilité au cœur de votre horaire de cours pour assurer ponctuellement des services en journée (sorties, theatre, lecture,...) ainsi qu'une présence régulière. Vous versez une participation mensuelle entre 125$ et 225$, selon les services offerts et les services utilisés.</span>
                                 </div>
-                                <div>
+                                <div class="form-pair">
                                     <input type="radio" name="housing" id="lgmtSolid" value="3" required>
                                     <label for="lgmtSolid">3-Logement solidaire, en échange de loyer et veille passive</label>
                                     <span>Vos études ne vous permettent pas de donner du temps, mais vous assurez une veille passive et des services spontanés. Vous versez une indemnité d'occupation mensuelle entre 300$ et 425$, selon les caractéristiques du logement</span>
@@ -179,16 +178,16 @@
                                 <div>
                                     <h3>Précision selon votre choix de logement</h3>
                                 </div>
-                                <div>
-                                    <label for="housing_2_availabilities">Pour la formule 2, vos disponibilités :</label>
+                                <div class="form-pair">
+                                    <label for="housing_2_availabilities">Pour la formule 2, vos disponibilités</label>
                                     <input type="text" id="housing_2_availabilities" name="housing_2_availabilities">
                                 </div>
-                                <div>
+                                <div class="form-pair">
                                     <label for="housing_3_budget">Pour la formule 3, votre budget maximum pour le loyer</label>
                                     <input type="number" id="housing_3_budget" name="housing_3_budget" min="0">
                                 </div>
-                                <div>
-                                    <label for="preferencesQuartier">* Si vous avez des préférences de quartier d'habitation (pour Montréal), précisez :</label>
+                                <div class="form-pair">
+                                    <label for="preferencesQuartier">* Si vous avez des préférences de quartier d'habitation (pour Montréal), précisez</label>
                                     <input type="text" id="preferencesQuartier" name="preferences">
                                 </div>
                             </div>
@@ -203,12 +202,12 @@
                                 <h2>Mot de passe</h2>
                             </div>
                             <div>
-                                <div>
-                                    <label for="password">Mot de passe :</label>
+                                <div class="form-pair">
+                                    <label for="password">Mot de passe</label>
                                     <input type="password" id="password" name="password" required>
                                 </div>
-                                <div>
-                                    <label for="password_repeat">Répéter le mot de passe :</label>
+                                <div class="form-pair">
+                                    <label for="password_repeat">Répéter le mot de passe</label>
                                     <input type="password" id="password_repeat" name="password_repeat" required>
                                 </div>
                             </div>
