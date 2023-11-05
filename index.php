39c6da4d312428530dda0b0490106436c8c87199
Romain Card <romain.card9@gmail.com>
2023-11-05 11:04:40 +0100

    nouvelle structure de projet

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+declare(strict_types=1);
+
+session_start();
+
+require_once('vendor/autoload.php');
+
+use App\ChezMamy\controllers\Router\Router;
+
+// Routeur
+$routeur = new Router();
+$routeur->routing($_GET, $_POST);
