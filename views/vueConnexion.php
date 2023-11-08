773fbc0264401ec6764b05487c2c7269e18067b6
Romain Card <romain.card9@gmail.com>
2023-11-08 20:39:35 +0100

    [views] Remplacement des textes par les valeurs du tableau de traductions [views/langs] Remplissage des fichiers fr.php et en.php pour les formulaires d'inscription

--- views/vueConnexion.php
+++ views/vueConnexion.php
@@ -9,16 +9,16 @@
                 <?php include('message.php'); ?>
                 <div class="form-group">
                     <div class="form-pair">
-                        <label for="login"><?=$traductions["register_id"]?></label>
+                        <label for="login"><?=$traductions["login_id"]?></label>
                         <input type="text" name="login" id="login"<?= ($login !== null) ? " value='{$login}'" : '' ?>>
                     </div>
                     <div>
-                        <label for="mdp"><?=$traductions["register_password"]?></label>
+                        <label for="mdp"><?=$traductions["login_password"]?></label>
                         <input type="password" name="password" id="mdp">
                     </div>
                 </div>
                 <div class="form-group">
-                    <input type="submit" id="login" class="bouton" value="<?=$traductions["register_connect"]?>">
+                    <input type="submit" id="login" class="bouton" value="<?=$traductions["login_connect"]?>">
                     <!--<p>Mot de passe oublié</p> -->
                 </div>
             </form>
