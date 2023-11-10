60dc2ded075c0d8608b7bef13025ed4e64386f4d
0KeTeR0 <louis.demeocq@gmail.com>
2023-11-10 17:47:48 +0100

    [RoutePosterOffres] post, fix l'erreur de chemin

--- controllers/Router/Route/RoutePosterOffres.php
+++ controllers/Router/Route/RoutePosterOffres.php
@@ -55,10 +55,10 @@ class RoutePosterOffres extends Route
                 // check si on a un path
                 if ($tmpFilePath != "") {
                     // crée le nouveau path
-                    $newFilePath = "public/img/offres/".date('d-m-Y_H:i:s', time())."_{$i}_{$_FILES['imagesOffre']['name'][$i]}";
+                    $newFilePath = "../../../public/img/offres/".date('d-m-Y_H:i:s', time())."_{$i}_{$_FILES['imagesOffre']['name'][$i]}";
 
                     // copie le fichier dans le dossier
-                    if (copy($_FILES['imagesOffre']['tmp_name'], $newFilePath))
+                    if (move_uploaded_file($_FILES['imagesOffre']['tmp_name'][$i], $newFilePath))
                     {
                         $chemins[] = $newFilePath;
                     }
