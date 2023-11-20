b6ba4a0703593083eff4bbfc4148f7d2d3f91788
Romain Card <romain.card9@gmail.com>
2023-11-20 12:58:11 +0100

    [SProprietesManager.php] ajout du fichier manquant

---
+++ models/Utilisateurs/Seniors/SProprietesManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\ChezMamy\models\Utilisateurs\Seniors;
+
+
+use App\ChezMamy\models\Model;
+
+/**
+ * Représentation de la table S_SITUATIONS de la BD
+ * @author Valentin Colindre
+ */
+class SProprietesManager extends Model
+{
+    public function getAll():array
+    {
+        $request = $this->execRequest("SELECT * FROM S_PROPRIETES");
+        $object_array = array();
+        foreach ($request->fetchAll() as $entree)
+        {
+            $objet = new SPropriete();
+            $objet->hydrate($entree);
+            $object_array[] = $objet;
+        }
+
+        return $object_array;
+    }
+}
