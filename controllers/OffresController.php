92d98627b76e47ee3fab0d200316fe1f0aae001f
Akt0o <valentin.colindre@gmail.com>
2023-11-21 15:55:31 +0100

    [OffresController] gererDemandesSenior - fix liste de besoin null.

--- controllers/OffresController.php
+++ controllers/OffresController.php
@@ -306,9 +306,10 @@ class OffresController
 
                     //On créer la liste des besoins
                     $besoins = array();
-                    foreach ($listeBesoins as $bs)
-                        foreach ($besoinsOffres as $besoinsOffre)
-                            if ($bs->getIdBesoin() == $besoinsOffre->getIdBesoin()) $besoins[] = $bs;
+                    if($listeBesoins!=null)
+                        foreach ($listeBesoins as $bs)
+                            foreach ($besoinsOffres as $besoinsOffre)
+                                if ($bs->getIdBesoin() == $besoinsOffre->getIdBesoin()) $besoins[] = $bs;
 
 
                     //On récupère les infos complémentaires
