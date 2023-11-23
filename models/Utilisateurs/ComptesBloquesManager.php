d52af8bd383b195c497e59261659960baf5053e1
Akt0o <valentin.colindre@gmail.com>
2023-11-23 17:59:34 +0100

    [CompteBloque] ajout de la classe et méthodes de base [ComptesBloquesManager] ajout de la classe et méthodes de base.

---
+++ models/Utilisateurs/ComptesBloquesManager.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\ChezMamy\models\Utilisateurs;
+
+
+use App\ChezMamy\models\Model;
+
+/**
+ * Le manager des entrées de la table COMPTES_BLOQUES
+ * @author Valentin Colindre
+ */
+class ComptesBloquesManager extends Model
+{
+    /**
+     * Récupère tous les comptes bloqués
+     * @return array tableau de tous les comptes bloqués
+     * @author Valentin Colindre
+     */
+    public function getAll(): array
+    {
+        $request = $this->execRequest("SELECT * FROM COMPTES_BLOQUES");
+
+        $besoins = [];
+        foreach ($request->fetchAll() as $besoin)
+        {
+            $bes = new CompteBloque();
+            $bes->hydrate($besoin);
+            $besoins[] = $bes;
+        }
+
+        return $besoins;
+    }
+
+    /**
+     * récupère le compteBloque d'idUtilisateur $idUtilisateur
+     * @param int $idCompteSenior l'IDutilisateur du compteBloque recherché
+     * @return CompteBloque|null renvoi le compteBloque ou rien s'il n'existe pas dans la DB.
+     * @author Valentin Colindre
+     */
+    public function getByIdUtilisateur(int $idUtilisateur):?CompteBloque{
+        $result = $this->execRequest("SELECT * FROM COMPTES_BLOQUES WHERE idUtilisateur=?",array($idUtilisateur))->fetch();
+        if($result!==false){
+            $utilisateur = new CompteBloque();
+            $utilisateur->hydrate($result);
+        }
+        else $utilisateur=null;
+
+        return $utilisateur;
+    }
+
+    /**
+     * bloque un compte
+     * @param int $idUtilisateur id de l'utilisateur bloqué
+     * @param string $raison raison du bloquage
+     * @return bool vrai si succès, faux si échec
+     * @author Valentin Colindre
+     */
+    public function creationCompteBloque(int $idUtilisateur, string $raison=null):bool
+    {
+        $result=false;
+        try {
+            if ($this->execRequest("INSERT INTO COMPTES_BLOQUES (idUtilisateur, raison) VALUES(?,?)", array($idUtilisateur, $raison)) !== false) {
+                $result = true;
+            }
+        }
+        catch (\Exception $e){
+            $result = false;
+        }
+        return $result;
+    }
+
+
+
+}
