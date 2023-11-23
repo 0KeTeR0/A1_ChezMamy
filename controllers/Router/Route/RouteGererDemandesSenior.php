bfe4f12c1f9dcd67a4f3c1e288cbe625dd51fe4f
0KeTeR0 <louis.demeocq@gmail.com>
2023-11-23 11:28:59 +0100

    [RouteBackofficeSignalement.php] post. Changer le parametres de la methode qui ne correspondait plus

--- controllers/Router/Route/RouteGererDemandesSenior.php
+++ controllers/Router/Route/RouteGererDemandesSenior.php
@@ -52,7 +52,8 @@ class RouteGererDemandesSenior extends Route
     protected function post(array $params = []): void
     {
         if(isset($params['idOffreToDelete'])){
-            $this->controller->supprimerDemandeSenior($this->getParam($params,'idOffreToDelete'));
+            $this->controller->supprimerOffres($this->getParam($params,'idOffreToDelete'));
+            $this->controller->gererDemandesSenior();
         }
     }
 }
